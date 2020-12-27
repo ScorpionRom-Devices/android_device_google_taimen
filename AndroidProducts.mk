4d244bde59b1475dd2bb0a7ce4960d3468591bec
Gibran Mather <gibranmather@gmail.com>
2020-12-27 03:27:19 +0000

    taimen: Add scorpion makefile and dependencies

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_taimen.mk
+    $(LOCAL_DIR)/scorpion_taimen.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_taimen-userdebug
+    scorpion_taimen-userdebug
